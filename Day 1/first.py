c9a6433705fb449ff286264d66dbc563d0e1d827
= <=>
2023-10-11 16:03:36 -0700

    Day 1 Project.

---
+++ Day 1/first.py
@@ -0,0 +1,18 @@
+# Udemy course : https://www.udemy.com/course/100-days-of-code
+
+# Day 1 :
+# Project Band Name Generator.
+
+print("Welcome to the Band Generator.")
+
+print("What's the name of the city you grew up in?")
+city = input()
+
+print("What is the name of a pet")
+pet = input()
+
+print("Your band name could be "+city+" "+pet)
+
+
+
+
