2b02db4dcf3825cae1d7183a8c645e59feff34db
= <=>
2023-10-11 16:37:53 -0700

    Day 2 Project.

---
+++ Day 2/day2.py
@@ -0,0 +1,13 @@
+print("Welcome to the tip Calculator.\nWhat was the total bill?")
+total_bill = float(input())
+print("What percentage tip would you like to give?")
+tip_percentage = float(input())
+print("How many people split the bill?")
+num = int(input())
+
+each = (total_bill + ((tip_percentage / 100) * total_bill)) / num
+
+print("Each person should pay:\n ", round(each, 2))
+
+
+
