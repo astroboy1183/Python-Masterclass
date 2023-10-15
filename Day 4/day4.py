b5dab2737bacf1c40d3b2f558d1b13f6d979640d
= <=>
2023-10-14 19:40:00 -0700

    Day 4 Project.

---
+++ Day 4/day4.py
@@ -0,0 +1,50 @@
+import random
+
+while(1):
+    print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+    rock = '''
+        _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)
+    '''
+
+    paper = '''
+        _______
+    ---'   ____)____
+              ______)
+              _______)
+             _______)
+    ---.__________)
+    '''
+
+    scissors = '''
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    '''
+    you = int(input())
+    Dict ={0:rock,1:paper,2:scissors}
+    print(Dict[you])
+
+    comp = random.randint(0,2)
+    print(Dict[comp])
+
+    if you <0 or you>=3:
+        print("Invalid Input.")
+
+    if you==comp:
+        print("Draw!")
+    elif you==0 and comp == 2:
+        print("You Win!")
+    elif you==2 and comp == 0:
+        print("Comp Wins!")
+    elif you>comp:
+        print("You Win!")
+    else:
+        print("Comp Wins!")
